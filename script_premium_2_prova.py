60a18c23b579b57aaa114c07960644fca3f418c8
latino123gl-ai <latino123gl@gmail.com>
2026-07-31 14:31:46 +0200

    Update script_premium_2_prova.py

--- script_premium_2_prova.py
+++ script_premium_2_prova.py
@@ -504,7 +504,7 @@ def main():
                     and not is_female_match(sex, home, away)   # solo maschi
                     and handi_val is not None
                     and -3.0 <= handi_val <= -0.0
-                    and minute in [24, 25, 26]
+                    and minute in [31, 32, 33]
                     and (hg, ag) in [
                         (1,0),(2,1),(0,1),(1,2),(1,1),(0,0)
                     ]  

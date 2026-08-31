e1afae97f348b2875e041ecff56e280d0e0cc444
latino123gl-ai <latino123gl@gmail.com>
2026-08-31 20:27:15 +0200

    Update script_premium_2_prova.py

--- script_premium_2_prova.py
+++ script_premium_2_prova.py
@@ -478,8 +478,8 @@ def main():
                     ]  
                     and datt_h >= 30                 # pericolosi casa
                     and on_h >= 3
-                    and off_h >= 3
-                    and tot_shots_h >= 6
+                    and off_h >= 2
+                    and tot_shots_h >= 5
                 ):
                     msg = (
                         "⚽️ NUOVO METODO X/2 GOL LINE 3.5\n\n"
@@ -542,7 +542,7 @@ def main():
                     and (hg, ag) in [
                         (1,0),(2,0),(3,0),(4,0),(2,1),(3,1),(4,1),(3,2),(4,2),(4,3)
                     ]    
-                    and datt_h >= 30
+                    and datt_h >= 35
                     and on_h >= 3
                     and off_h >= 3
                     and tot_shots_h >= 6

ff57276c8ec1c17fd727d826e43b6842dc0ed83a
latino123gl-ai <latino123gl@gmail.com>
2026-09-15 09:24:17 +0200

    Update script_premium_2_prova.py

--- script_premium_2_prova.py
+++ script_premium_2_prova.py
@@ -545,7 +545,7 @@ def main():
                     and (hg, ag) in [
                         (1,0),(2,0),(3,0),(4,0),(2,1),(3,1),(4,1),(3,2),(4,2),(4,3)
                     ]    
-                    and datt_h >= 35
+                    and datt_h >= 30
                     and on_h >= 3
                     and off_h >= 2
                     and tot_shots_h >= 5
